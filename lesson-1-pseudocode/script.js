66d8239a86ad367b36417042a31568de8a4da23e
ChristopherAhlim <118709622+ChrisAlan9903@users.noreply.github.com>
2026-04-28 21:41:57 +0800

    feat: added code for JS and links

---
+++ lesson-1-pseudocode/script.js
@@ -0,0 +1,16 @@
+// 1. target element that need to add class
+// 2. Write logic to add class
+
+const toggleClassBtn = document.querySelector(".download-btn");
+const icons = document.querySelectorAll(".fa-brands");
+const name = document.querySelector("h3");
+
+// creating the logic inside a function
+
+toggleClassBtn.addEventListener("click", () => {
+  icons.forEach((i) => {
+    i.classList.toggle("state-colour");
+  });
+
+  name.classList.toggle("colour");
+});
