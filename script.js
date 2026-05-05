0a4136c28484f57485927d64ffe7707b908faf1b
ChristopherAhlim <118709622+ChrisAlan9903@users.noreply.github.com>
2026-05-05 16:00:09 +0800

    feat: input lesson

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const toggleEnter = document.querySelector(".enter-btn");
 const num = document.querySelector("h1");
+const input = document.querySelector("input");
+
+let inputValueHolder = 0;
+
+toggleEnter.addEventListener("click", () => {
+  // 1. grab to value from input, and set it into inputValueHolder
+  //   inputValueHolder = input.value;
+
+  // validate input
+  if (input.value == null || input.value == "") {
+    alert("please enter some input first");
+  }
+
+  // 2. replace the num with the data inside inputValueHolder
+  //   num.textContent = inputValueHolder;
+
+  num.textContent = input.value;
+});
