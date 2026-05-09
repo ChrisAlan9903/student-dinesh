7da941a804c05c5e199f19660d295a30be166b9a
lnitial-D <munyanyo550@gmail.com>
2026-05-09 16:44:44 +0800

    feat: updated ui

---
+++ lesson6-list/tutor/Student/script.js
@@ -0,0 +1,5 @@
+const lifeGoalsInput = document.getElementById("life-goals-input");
+const todoListInput = document.getElementById("todo-list-input");
+const enterBtnLifeGoals = document.getElementsByClassName(
+  "enter-btn-life-goals",
+);
