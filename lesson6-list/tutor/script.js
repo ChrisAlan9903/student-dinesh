4c8cf417ef297924349a350144d199500fc81079
ChristopherAhlim <118709622+ChrisAlan9903@users.noreply.github.com>
2026-05-12 14:19:06 +0800

    feat: edit code for id

--- lesson6-list/tutor/script.js
+++ lesson6-list/tutor/script.js
@@ -1,10 +1,11 @@
+const lifeGoalsInput = document.getElementById("life-goals-input");
 const todoListInput = document.getElementById("todo-list-input");
 const enterBtnLifeGoals = document.getElementsByClassName(
   "enter-btn-life-goals",
 );
 const ul = document.querySelector("ul");
 
-// const lifeGoalsInput = document.querySelector("#life-goals-input").value;
+const lifeGoalsInput = document.querySelector("#life-goals-input").value;
 
 // const input1 = document.getElementById("life-goals-input");
 
@@ -12,7 +13,6 @@ const enterBtnLifeGoalsSingle = enterBtnLifeGoals[0];
 
 enterBtnLifeGoalsSingle.addEventListener("click", () => {
   // get input value
-  const lifeGoalsInput = document.getElementById("life-goals-input");
   let inputValue = lifeGoalsInput.value;
 
   // innerHTML
