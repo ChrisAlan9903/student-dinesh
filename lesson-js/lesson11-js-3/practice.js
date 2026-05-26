2e18e327032ef452e29f30f6522b0480964ac96c
lnitial-D <munyanyo550@gmail.com>
2026-05-26 22:24:00 +0800

    feat: added age checker

--- lesson-js/lesson11-js-3/practice.js
+++ lesson-js/lesson11-js-3/practice.js
@@ -18,3 +18,27 @@ console.log(oddOrEven(3));
 // 3. create if-else and check the logic output. if output = even, return "even". if output = odd, return "odd"
 
 // Q2. Write a program that check whether a person is teenager or adult. Hint (teenager age between 13-21, adult is 21 above)
+
+function ageChecker(age) {
+  if (age < 12) {
+    return "Kid";
+  } else if (age >= 13 && age <= 21) {
+    return "Teenager";
+  } else {
+    return "Adult";
+  }
+}
+
+console.log(ageChecker(20));
+
+function ageChecker2(age) {
+  if (age > 21) {
+    return "Adult";
+  } else if (age >= 13 && age <= 21) {
+    return "Teenager";
+  } else {
+    return "Kid";
+  }
+}
+
+console.log(ageChecker2(25));
