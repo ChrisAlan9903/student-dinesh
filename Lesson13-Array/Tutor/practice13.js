c84c522d53a60ffb25da7f791ceda7294bcf8309
lnitial-D <munyanyo550@gmail.com>
2026-07-23 21:27:06 +0800

    completed exercise 13

--- Lesson13-Array/Tutor/practice13.js
+++ Lesson13-Array/Tutor/practice13.js
@@ -4,6 +4,7 @@
 //
 // Example:
 // ["Apple", "Banana", "Orange", "Mango", "Grape"]
+let fruits = ["Banana", "Orange", "Mango", "Grape"];
 
 // Question 2
 // Create an array called "scores".
@@ -11,14 +12,17 @@
 //
 // Example:
 // [80, 90, 75, 60, 100]
+let scores = [80, 90, 75, 60, 100];
 
 // Question 3
 // Create an array called "colors".
 // Store 4 different colors inside the array.
+let colors = ["Red", "Pink", "Black", "Blue", "Beige"];
 
 // Question 4
 // Create an array called "movies".
 // Store 3 movie names inside the array.
+let movies = ["John Wick", "Fast and Furious", "Rush Hour"];
 
 // Question 5
 // Create an array called "studentInfo".
@@ -29,26 +33,31 @@
 //
 // Example:
 // ["Ali", 15, "Form 3"]
+let studentInfo = ["Ali", 15, "Form 3"];
 
 // Question 1
 // Print the first fruit from the array.
 
 let fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits[0]);
 
 // Question 2
 // Print the second number from the array.
 
 let numbers = [10, 20, 30, 40];
+console.log(numbers[1]);
 
 // Question 3
 // Print the last color from the array.
 
 let colors = ["Red", "Blue", "Green", "Yellow"];
+console.log(colors[3]);
 
 // Question 4
 // Print the third movie name.
 
 let movies = ["Avatar", "Titanic", "Frozen", "Batman"];
+console.log(movies[2]);
 
 // Question 5
 // Create a variable called "highestScore".
@@ -56,28 +65,35 @@ let movies = ["Avatar", "Titanic", "Frozen", "Batman"];
 // Then print it.
 
 let scores = [70, 80, 90, 100];
+let highestScore = scores[3];
+console.log(highestScore);
 
 // Question 1
 // Change "Apple" into "Mango".
 
 let fruits = ["Apple", "Banana", "Orange"];
+fruits[0] = "Mango";
 
 // Question 2
 // Change the first score to 100.
 
 let scores = [70, 80, 90];
+scores[0] = 100;
 
 // Question 3
 // Change the third color to "Black".
 
 let colors = ["Red", "Blue", "Green"];
+colors[2] = "Black";
 
 // Question 4
 // Change the second movie name.
 
 let movies = ["Avatar", "Titanic", "Frozen"];
+movies[1] = "John Wick";
 
 // Question 5
 // Change the student's age to 16.
 
 let studentInfo = ["Ali", 15, "Form 3"];
+studentInfo[1] = 16;
